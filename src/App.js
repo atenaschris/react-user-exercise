0ec1a03d8a757f29e3b5d83df4f51b632daaf0af
Christofer cardone <chriscardone69@gmail.com>
2021-09-22 22:19:23 +0200

    React lazy import added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import "./App.css";
 import { Switch, Route } from "react-router";
-import DetailPage from "./pages/DetailPage";
+
+import React, { Suspense } from "react";
 
 import Layout from "./components/UI/Layout";
 
 import Welcome from "./pages/Welcome";
-import NewUser from './pages/NewUser';
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const DetailPage = React.lazy(() => 
+  import("./pages/DetailPage")
+);
+
+const NewUser = React.lazy(() => 
+  import("./pages/NewUser")
+);
 
 function App() {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Welcome/>
-        </Route>
-        <Route path="/detail/:userId">
-          <DetailPage/>
-        </Route>
-        <Route path="/new-user">
-          <NewUser/>
-        </Route>
-      </Switch>
-    </Layout>
+    <Suspense
+      fallback={
+        <div>
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Welcome />
+          </Route>
+          <Route path="/detail/:userId">
+            <DetailPage />
+          </Route>
+          <Route path="/new-user">
+            <NewUser />
+          </Route>
+        </Switch>
+      </Layout>
+    </Suspense>
   );
 }
 
